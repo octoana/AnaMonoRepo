cfb7437a0e1a13ad35f28ccfb29f0dc30e121cbe
Ana Scolari <apsscolari@github.com>
2025-04-02 20:31:58 -0700

    new python file within source dir

---
+++ PythonScript/src/test_script.py
@@ -0,0 +1,23 @@
+# APS
+# test ana
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#to trigger secret scanning
+user = 'test'
+password = 'Password1234'
+
+google_api_token = "AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q"
+
+def say_hello():
+    print('hello world - test Ana inside the source folder')
+
+    # critical vuln example
+    user_input = input("Enter filename: ")
+    with open(user_input, 'r') as file:  # Vulnerable to directory traversal
+        content = file.read()
+
+# main
+if __name__ == '__main__':
+    say_hello()
